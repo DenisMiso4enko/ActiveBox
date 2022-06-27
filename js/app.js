5765bcd0f87e4b8765a4a6ea3a394e0ce24049e9
DenisMiso4enko <denrrmisochenko17@gmail.com>
2022-06-27 19:10:06 +0300

    remove content

--- js/app.js
+++ js/app.js
@@ -8,15 +8,20 @@ $('.slider').slick({
 // Ищем нужные элементы бургер и меню
 const burger = document.querySelector('.burger')
 const menu = document.querySelector('.header__menu__list')
+
+const headerContent = document.querySelector('.content__wrapper')
 // Вещаем событие на бургер и добовляем переключатель с классом active
 burger.addEventListener('click', () => {
     burger.classList.toggle('active')
     menu.classList.toggle('active')
-    
+    headerContent.classList.toggle('hide')
 })
 // При нажатии на сылку из меню удаляем класс active
 document.querySelectorAll('.menu__list').forEach(n => n.addEventListener('click', () => {
     burger.classList.remove('active')
     menu.classList.remove('active')
+    headerContent.toggle('hide')
 }))
+
+
 
